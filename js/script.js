e6a424d465be430b3d9b88f8926111b2001fb3d0
Eric D. Fields <ericdfields@gmail.com>
2011-06-28 17:51:39 -0400

    prev

--- js/script.js
+++ js/script.js
@@ -7,19 +7,23 @@ var $main,$footer,$prevArticles,$currArticles,$nextArticles;
 counter = 5;
 $main = $('#main');
 $footer = $('#footer');
-$footer.append('<button id="x_next">&rarr;</button>');
+$footer.append('<button id="x_prev">&larr;</button><button id="x_next">&rarr;</button>');
 
-$main.prepend('<div id="previous-articles" /><div id="current-article" /><div id="next-articles" />');
-$prevArticles = $('#previous-articles'); 
-$currArticle = $('#current-article');
-$nextArticles = $('#next-articles');
+// $main.prepend('<div id="previous-articles" /><div id="current-article" /><div id="next-articles" />');
+// $prevArticles = $('#previous-articles'); 
+// $currArticle = $('#current-article');
+// $nextArticles = $('#next-articles');
 
-$main.find('article:not(:first-child)').appendTo($('#next-articles'));
-$main.find('article:first-child').appendTo($('#current-article'));
+$main.find('article:not(:first-child)').addClass('next')
+$main.find('article:first-child').addClass('current');
 
 $('#x_next').live('click',function(){                            
   counter++;
-  $currArticle.find('article').appendTo($prevArticles);
-  $currArticle.append($nextArticles.find('article:first-child'));
-  $nextArticles.append('<article><header>' + counter + '</header></article>');
+  $main.find('.current').removeClass('current un-prev').addClass('prev').end()
+    .find('.next').first().addClass('current').removeClass('next').end();
+  $main.append('<article class="next"><header>' + counter + '</header></article>');
+})
+$('#x_prev').live('click',function(){                            
+  $main.find('.current').removeClass('current un-prev').addClass('next').end()
+    .find('.prev').last().addClass('current un-prev').removeClass('prev').end();
 })
