42fb9248c30be58aa28f37cff6200865a553dfef
Eric D. Fields <ericdfields@gmail.com>
2011-06-28 13:15:11 -0400

    zexyback

--- js/script.js
+++ js/script.js
@@ -3,13 +3,23 @@
 */
          
 var counter;          
-var $main,$footer;  
+var $main,$footer,$prevArticles,$currArticles,$nextArticles;  
 counter = 5;
 $main = $('#main');
 $footer = $('#footer');
 $footer.append('<button id="x_next">&rarr;</button>');
+
+$main.prepend('<div id="previous-articles" /><div id="current-article" /><div id="next-articles" />');
+$prevArticles = $('#previous-articles'); 
+$currArticle = $('#current-article');
+$nextArticles = $('#next-articles');
+
+$main.find('article:not(:first-child)').appendTo($('#next-articles'));
+$main.find('article:first-child').appendTo($('#current-article'));
+
 $('#x_next').live('click',function(){                            
   counter++;
-  $main.find('article:first-child').remove().end()
-    .append('<article><header>' + counter + '</header></article>');
+  $currArticle.find('article').appendTo($prevArticles);
+  $currArticle.append($nextArticles.find('article:first-child'));
+  $nextArticles.append('<article><header>' + counter + '</header></article>');
 })
